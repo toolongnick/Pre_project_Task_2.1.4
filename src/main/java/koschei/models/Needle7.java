c5220bd771fddaf0b097f10af2c9083f7a33b01e
Sergey Tashkin <toolongnick@mail.ru>
2021-12-15 16:18:05 +0300

    added class Death8 to the chain

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,12 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
 
+    @Autowired
+    private Death8 death;
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле " + death.toString();
     }
 }
